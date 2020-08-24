b072b8fb9c2710b6098bb411a7441187a63b3a55
Stanwang1210 <ch995308@gmail.com>
2020-08-24 14:51:09 +0800

    'upload'

--- logger.py
+++ logger.py
@@ -1,14 +1,26 @@
-import tensorflow as tf
-
-
 class Logger(object):
-    """Tensorboard logger."""
-
     def __init__(self, log_dir):
-        """Initialize summary writer."""
+        """Create a summary writer logging to log_dir."""
         self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        """Add scalar summary."""
+        """Log a scalar variable."""
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
         self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        # self.writer.add_summary(summary, step)
+
+    def list_of_scalars_summary(self, tag_value_pairs, step):
+        """Log scalar variables."""
+        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
+        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            for tag, value in tag_value_pairs:
+                tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
+        # self.writer.add_summary(summary, step)
